f5904cc9879c18a22d5a22ca97614acd011cc5d2
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-08-17 10:08:59 +0200

    ggaparc

--- R/ggaparc.R
+++ R/ggaparc.R
@@ -1,53 +1,49 @@
 #'
 
-ggaparc = function(){
+ggaparc = function(data){
   ### Ids corresponding to parcelations in this image
   GeoAparc = cbind(
-    id = c(0, 1, 4,7, 8,9,15,17,20,21,22,23,28,32,34,39,41,49),
+    id = c(0, 1, 4, 7, 8,9,15,17,20,21,22,23,28,32,34,39,41,49),
     area = c("parstriangularis","smn","motor","ipl","stg","mtg","loc","rmf",
              "lvmpfc","bankssts","insula","spc","itg","visual","cmf","parsorbitalis",
              "supfrontal","transverse")
   ) %>% as.data.frame(stringsAsFactors=F)
 
 
+  if(!missing(data)){
+    if(!"area" %in% names(data)){
+      str = paste(GeoAparc$area, collapse=", ")
+      stop(paste0("Supplied data must have an 'area' column, and must have one of following areas: ",str))
+    }
+
+    unknown=!(data$area %in% GeoAparc$area)
+    if(any(unknown)){
+      warning(paste0("Data contains unknown area ", data$area[unknown]))
+    }
+  }
+
   ##### main routine #####
   #Open geo object (shape file)
   fileLoc = system.file("data","geobrain", package = "MOAS")
-  geobrainOGR <- rgdal::readOGR(dsn = "fileLoc", layer = "geobrain")
+  geobrainOGR <- rgdal::readOGR(dsn = fileLoc, layer = "geobrain", verbose = F)
 
-  #transform to data.frame
+  # transform to data.frame
   geobrain <- ggplot2::fortify(geobrainOGR, region="GEO_ID")
-  #utils::head(geobrain)
 
-
-  ## RAW PLOT commentary:
-  # as the raw plot is far from perfect there are several polygons that do not correspond
-  # to any aparc. Yet due to the way the image is transformed most of the lower indices should correspond
-  # to real regions
-
-  #### some cleaning can be done #####
-  # to remove unlikely polygons: geobrain2 <- geobrain[as.numeric(geobrain$id) < 50,]
-  # to check individual regions: geobrain2 <- geobrain[as.numeric(geobrain$id) == 12]
-  # to retain more complex polygons: sort(table(as.numeric(geobrain$id)))
-
-  # here the following combination worked well - though some manual labour
-#
-#   geobrain2 <- geobrain[as.numeric(geobrain$id) < 50,]
-#   sort(table(as.numeric(geobrain$id)))
-
+  # Do some cleanup of the data
   geobrain2 <- geobrain %>%
-    dplyr::filter(id %in% GeoAparc$id)
+    dplyr::left_join(GeoAparc, by="id") %>%
+    dplyr::filter(!is.na(area)) %>%
+    rename(Region=id)
 
   ggplot2::ggplot(data = geobrain2) +
     ggplot2::geom_polygon(
       ggplot2::aes(x = long,
                    y = lat,
-                   group = as.factor(id),
-                   fill=as.numeric(id)), color = "white", size = .5) +
+                   group = area),
+      color = "white", size = .3) +
     coord_fixed() +
-    ggplot2::theme_classic()  +
-    ggplot2::theme(legend.position = "none")
-
+    ggplot2::theme_void()
 }
 
 
